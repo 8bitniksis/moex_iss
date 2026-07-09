3199082e55f64dd5d2cf2c32fb83582181bc301f
Nikita <zalesov.anatoly@yandex.ru>
2026-07-09 16:42:45 +0400

    feat: Added new tests

---
+++ tests/test_limiter.py
@@ -0,0 +1,30 @@
+import time
+
+from moex_iss.limiter import RateLimiter
+
+
+
+def test_rate_limiter():
+
+
+    limiter = RateLimiter(
+        rate=10
+    )
+
+
+    start=time.time()
+
+
+    limiter.wait()
+
+    limiter.wait()
+
+
+    elapsed = (
+        time.time()
+        -
+        start
+    )
+
+
+    assert elapsed >= 0.1
