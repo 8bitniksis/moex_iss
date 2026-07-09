6ff83b709bc8b79aa9730700ec82cef184f4937e
Nikita <zalesov.anatoly@yandex.ru>
2026-07-09 16:29:15 +0400

    feat: Update execption by rate limiter

--- moex_iss/exceptions.py
+++ moex_iss/exceptions.py
@@ -1,25 +1,79 @@
 class ISSException(Exception):
     """
-    Base ISS exception
+    Base exception for MOEX ISS client
     """
     pass
 
 
 
-class ISSConnectionError(ISSException):
+class ISSConnectionError(
+    ISSException
+):
+    """
+    Network problems
+    """
     pass
 
 
 
-class ISSAuthenticationError(ISSException):
+class ISSAuthenticationError(
+    ISSException
+):
+    """
+    Authentication failed
+    """
     pass
 
 
 
-class ISSResponseError(ISSException):
+class ISSResponseError(
+    ISSException
+):
+    """
+    Invalid response from ISS
+    """
     pass
 
 
 
-class ISSPaginationError(ISSException):
+class ISSServerError(
+    ISSException
+):
+    """
+    HTTP 5xx errors from MOEX ISS
+    """
+
+    def __init__(
+        self,
+        status_code,
+        message=None
+    ):
+
+        self.status_code = status_code
+
+        self.message = (
+            message
+            or
+            f"MOEX ISS server error {status_code}"
+        )
+
+        super().__init__(
+            self.message
+        )
+
+
+
+class ISSRateLimitError(
+    ISSException
+):
+    """
+    HTTP 429 Too Many Requests
+    """
+    pass
+
+
+
+class ISSPaginationError(
+    ISSException
+):
     pass
