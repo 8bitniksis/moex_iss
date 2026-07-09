12113fc0ba73591245186411ab743e99a1598fca
Nikita <zalesov.anatoly@yandex.ru>
2026-07-09 17:27:24 +0400

    chore: Added CI and fix examples

--- examples/06_async_download.py
+++ examples/06_async_download.py
@@ -3,43 +3,30 @@ import asyncio
 from moex_iss import AsyncISSClient
 
 
-
 URLS = [
 
-"https://iss.moex.com/iss/engines.json",
+    "https://iss.moex.com/iss/engines.json",
 
-"https://iss.moex.com/iss/markets.json"
+    "https://iss.moex.com/iss/engines/stock/markets.json"
 
 ]
 
 
-
 async def main():
-
 
     async with AsyncISSClient() as client:
 
-
         tasks = [
-
             client.get_json(url)
-
             for url in URLS
-
         ]
-
 
         result = await asyncio.gather(
             *tasks
         )
 
-
         for item in result:
-
-            print(
-                item.keys()
-            )
-
+            print(item.keys())
 
 
 asyncio.run(main())
--- examples/07_export_parquets.py
+++ examples/07_export_parquets.py
@@ -1,26 +1,26 @@
-from moex_iss import ISSClient
+from pathlib import Path
 
+from moex_iss import ISSClient
 
 
 def main():
 
-    client = ISSClient()
+    Path("data").mkdir(
+        exist_ok=True
+    )
 
+    client = ISSClient()
 
     df = client.candles_df(
         "SBER",
         interval=10
     )
 
-
     df.to_parquet(
         "data/sber_10m.parquet"
     )
 
-
-    print(
-        "saved"
-    )
+    print("saved")
 
 
 if __name__ == "__main__":
