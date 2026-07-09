c90408e8e5e9d51335f634f96dbceb84c9543358
Nikita <zalesov.anatoly@yandex.ru>
2026-07-09 16:16:56 +0400

    feat: Added new tests and requirements files

---
+++ tests/test_http_client.py
@@ -0,0 +1,34 @@
+import responses
+
+from moex_iss import ISSClient
+
+
+@responses.activate
+def test_client_get_json():
+
+
+    url = (
+        "https://iss.moex.com"
+        "/iss/test.json"
+    )
+
+
+    responses.add(
+        responses.GET,
+        url,
+        json={
+            "test":"ok"
+        },
+        status=200
+    )
+
+
+    client = ISSClient()
+
+
+    result = client.session.get(
+        url
+    ).json()
+
+
+    assert result["test"]=="ok"
